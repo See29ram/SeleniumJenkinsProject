cb754da8fb1738b24218e3ff821760cae947620b
sea29ramUbuntu <sea29ram@gmail.com>
2020-10-02 15:00:17 +0530

    added surefire plugin

---
+++ src/test/java/org/SeleniumHq/ZapposTest.java
@@ -0,0 +1,42 @@
+package org.SeleniumHq;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ZapposTest {
+
+    private WebDriver webDriver;
+
+    @BeforeTest
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.get("https://www.zappos.com/");
+        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void launchGoogle() {
+
+        webDriver.findElement(By.id("searchAll")).sendKeys("Bags");
+        //webDriver.findElement(By.linkText("Gmail")).click();
+        Uninterruptibles.sleepUninterruptibly(5, TimeUnit.SECONDS);
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        webDriver.quit();
+    }
+
+
+}
